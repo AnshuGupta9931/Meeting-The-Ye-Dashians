40fe7ced1dc33aa9b655b4e921fb9efad2fd7196
Anshu Gupta <anshugupta4142009@gmail.com>
2025-04-01 21:56:40 +0530

    updated pages routed in main file

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,10 +6,18 @@ import App from './App.jsx'
 import {createBrowserRouter, createRoutesFromElements, RouterProvider,Route} from "react-router-dom"
 import Layout from "./Layout"
 import { Home } from './pages/Home.jsx'
+import {Login} from "./pages/Login.jsx"
+import { Signup } from './pages/Signup.jsx'
+import { ResetPassword } from './pages/ResetPassword.jsx'
+import { VerifyEmail } from './pages/VerifyEmail.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout/>}>
         <Route path='' element={<Home/>}/>
+        <Route path='login' element={<Login/>}/>
+        <Route path='signup' element={<Signup/>}/>
+        <Route path='reset-password' element={<ResetPassword/>}/>
+        <Route path='verify-email' element={<VerifyEmail/>} />
       </Route>
   )
 )
