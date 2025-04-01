cc3ef075ff716010286a4f358c37684b8bb613b2
Aman20-debug <amanpoddar08072003@email.com>
2025-04-01 20:34:42 +0530

    Added ResetPasswordToken and ResetPassword Routes

--- Backend/src/routes/User.js
+++ Backend/src/routes/User.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 // Import the required controllers and middleware functions
 import { login, signup, sendOTP, changePassword } from "../controllers/Auth.js";
+import { resetPassword, resetPasswordToken } from "../controllers/ResetPassword.js";
 
 import {auth} from "../middlewares/auth.js"
 
@@ -24,5 +25,16 @@ router.post("/sendotp", sendOTP)
 
 // Route for Changing the password
 router.post("/changepassword", auth, changePassword)
+
+// ********************************************************************************************************
+//                                      Reset Password
+// ********************************************************************************************************
+
+// Route for generating a reset password token
+router.post("/reset-password-token", resetPasswordToken)
+
+// Route for resetting user's password after verification
+router.post("/reset-password", resetPassword)
+
 
 export default router;
